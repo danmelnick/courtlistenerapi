9de8d166396ecabdf920a4b54cd74b6d7afa6160
Christian <christian.stanfield@gmail.com>
2021-03-17 13:35:08 -0500

    WIP Experiment with parties/attorneys endpoints

--- ruby-client.rb
+++ ruby-client.rb
@@ -1,9 +1,12 @@
 require 'net/https'
 require 'json'
+require 'pry'
+require 'csv'
 
 BASE_HOST = 'www.courtlistener.com'
 API_PATH = '/api/rest/v3/'
 DOCKETS_PATH = API_PATH + 'dockets/'
+PARTIES_PATH = API_PATH + 'attorneys/'
 
 
 def get_results(path, query = nil)
@@ -13,7 +16,8 @@ def get_results(path, query = nil)
   uri.port = 443
 
   req = Net::HTTP::Get.new(uri)
-  req.basic_auth 'neweraadrapi', 'Z58YCLBdarqaYNK'
+  # req.basic_auth 'neweraadrapi', '7sV2i7QTJQgzmsc'
+  req['Authorization'] = 'Token XXX'
 
   res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
     http.request(req)
@@ -24,8 +28,20 @@ def get_results(path, query = nil)
   response['results']
 end
 
-data = get_results(DOCKETS_PATH, {
-  nature_of_suit: '840 Trademark',
-  'date_created__lte' => '2020-07-25T00:00:00Z',
-  'date_created__gte' => '2019-01-01T00:00:00Z'
-})
+# data = get_results(DOCKETS_PATH, {
+#   nature_of_suit: '840 Trademark',
+#   'date_created__lte' => '2020-07-25T00:00:00Z',
+#   'date_created__gte' => '2019-01-01T00:00:00Z'
+# })
+
+# data = get_results(PARTIES_PATH, id: '1900821')
+
+def parse_csv(file_path)
+  csv = CSV.read(file_path, headers: true)
+  csv.each do |row|
+    docket_id = row['DOCKET']
+    get_results(PARTIES_PATH, id: docket_id)
+  end
+end
+
+parse_csv('data.csv')
